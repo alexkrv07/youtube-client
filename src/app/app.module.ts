4eb51a6bd74ed3a150b5b8db4c35b410ad5bd435
Oleksii Korovushkin <alexkrv07@gmail.com>
2022-03-27 14:38:50 +0300

    add base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchResultListComponent } from './search/search-result-list/search-result-list.component';
+import { SearchResultItemComponent } from './search/search-result-item/search-result-item.component';
+import { FilteringBlockComponent } from './header/filtering-block/filtering-block.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SearchResultListComponent,
+    SearchResultItemComponent,
+    FilteringBlockComponent,
   ],
   imports: [
     BrowserModule,
